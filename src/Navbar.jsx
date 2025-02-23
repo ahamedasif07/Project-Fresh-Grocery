2c67507e4ede84a417bf3fd432f6180723f168e7
rxasif07 <rxasif31@gmail.com>
2025-02-23 08:14:19 +0600

    section container setup in nav top

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import SectionContainer from "./components/SectionContainer/SectionContainer";
 
 const Navbar = () => {
   return (
     <div>
       {/* top navbar start */}
-      <div className="bg-[#F3FAF5] py-4 flex  justify-between place-items-center">
-        <div className="flex place-items-center gap-7 ">
-          <h3>Account</h3>
-          <h3>Track Order</h3>
-          <h3>Support</h3>
-        </div>
-        <div className="flex place-items-center gap-7">
-          <h3 className="">+ 00645 4568</h3>
+      <div className="bg-[#F3FAF5] py-4 ">
+        <SectionContainer>
+          <div className="flex  justify-between place-items-center">
+            <div className="flex place-items-center gap-7 ">
+              <h3>Account</h3>
+              <h3>Track Order</h3>
+              <h3>Support</h3>
+            </div>
+            <div className="flex place-items-center gap-7">
+              <h3 className="">+ 00645 4568</h3>
 
-          <h3>Youremai@gmail.com</h3>
-        </div>
+              <h3>Youremai@gmail.com</h3>
+            </div>
+          </div>
+        </SectionContainer>
       </div>
       {/* top navbar end */}
     </div>
---
+++ src/components/SectionContainer/SectionContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SectionContainer = ({ children }) => {
+  return (
+    <div className="max-w-screen-2xl mx-auto md:px-[80px] ">{children}</div>
+  );
+};
+
+export default SectionContainer;
