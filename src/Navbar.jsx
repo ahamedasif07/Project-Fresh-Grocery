036471ee22696ba313038183f773bf4535caa73f
rxasif07 <rxasif31@gmail.com>
2025-02-23 08:05:36 +0600

    top nav set

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      {/* top navbar start */}
+      <div className="bg-[#F3FAF5] py-4 flex  justify-between place-items-center">
+        <div className="flex place-items-center gap-7 ">
+          <h3>Account</h3>
+          <h3>Track Order</h3>
+          <h3>Support</h3>
+        </div>
+        <div className="flex place-items-center gap-7">
+          <h3 className="">+ 00645 4568</h3>
+
+          <h3>Youremai@gmail.com</h3>
+        </div>
+      </div>
+      {/* top navbar end */}
+    </div>
+  );
 };
 
 export default Navbar;
