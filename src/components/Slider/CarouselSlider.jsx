95563b737fbab6578621749a244db5d89a08b0bf
rxasif07 <rxasif31@gmail.com>
2025-03-22 07:59:44 +0600

    slider bg image sets

--- src/components/Slider/CarouselSlider.jsx
+++ src/components/Slider/CarouselSlider.jsx
@@ -6,6 +6,7 @@ import heroImgOne from "../../../public/assets/hero-img-1.webp";
 import heroImgTwo from "../../../public/assets/hero-img-2.webp";
 import heroImgTheree from "../../../public/assets/hero-img-3.webp";
 import PrimaryButton from "../PrymaryButton/PrimaryButton";
+import SectionContainer from "../SectionContainer/SectionContainer";
 
 const CarouselSlider = () => {
   const settings = {
@@ -20,80 +21,86 @@ const CarouselSlider = () => {
 
   return (
     <div>
-      <div className="py-10 relative z-[-1]">
-        {" "}
-        {/* Lower z-index */}
-        <Slider {...settings} className="relative z-0">
-          <div className="!flex flex-col-reverse md:flex-row p-6 place-items-center w-full">
-            <div className="md:w-1/2 text-center md:text-left">
-              <h3 className="text-green-600 text-[30px] font-semibold">
-                FRESH GROCERY
-              </h3>
-              <h2 className="text-3xl md:text-4xl lg:text-6xl font-extrabold my-2 leading-[1.5] -tracking-wide">
-                There's you can Buy your all of Grocery Products.
-              </h2>
-              <PrimaryButton
-                text="Shop Now"
-                colour="[#FFBB38]"
-                hoverColour="black"
-              />
-            </div>
-            <div className="md:w-1/2 flex justify-center">
-              <div className="h-[450px] w-[450px]">
-                <img src={heroImgOne} alt="Grocery" className="" />
-              </div>
-            </div>
-          </div>
-
-          {/* Slide 2 */}
-          <div className="!flex flex-col-reverse md:flex-row place-items-center w-full p-6">
-            <div className="md:w-1/2 text-center md:text-left">
-              <h3 className="text-green-600 text-[30px] font-semibold">
-                HEALTHY FOODS
-              </h3>
-              <h2 className="text-3xl md:text-4xl lg:text-6xl font-extrabold my-2 leading-[1.5] -tracking-wide">
-                We Provide Fresh And Organic Food to Your Door.
-              </h2>
-              <PrimaryButton
-                text="Shop Now"
-                colour="[#FFBB38]"
-                hoverColour="black"
-              />
-            </div>
-            <div className="md:w-1/2 flex justify-center">
-              <div className="max-h-[450px] mx-auto max-w-[450px]">
-                <img src={heroImgTwo} alt="Grocery" className="w-full h-auto" />
-              </div>
-            </div>
-          </div>
-
-          {/* Slide 3 */}
-          <div className="!flex flex-col-reverse md:flex-row w-full place-items-center p-6">
-            <div className="md:w-1/2 text-center md:text-left">
-              <h3 className="text-green-600 text-[30px] font-semibold">
-                HEALTHY FOODS
-              </h3>
-              <h2 className="text-3xl md:text-4xl lg:text-6xl font-extrabold my-2 leading-[1.5] -tracking-wide">
-                You Can Buy All The Grocery Items Hassle-Free.
-              </h2>
-              <PrimaryButton
-                text="Shop Now"
-                colour="[#FFBB38]"
-                hoverColour="black"
-              />
-            </div>
-            <div className="md:w-1/2 flex justify-center">
-              <div className="max-h-[450px] mx-auto max-w-[450px]">
-                <img
-                  src={heroImgTheree}
-                  alt="Grocery"
-                  className="w-full h-auto"
+      <SectionContainer>
+        <div className="py-10 relative z-[-1]">
+          {" "}
+          {/* Lower z-index */}
+          <Slider {...settings} className="relative z-0">
+            <div className="!flex flex-col-reverse md:flex-row p-6 place-items-center w-full">
+              <div className="md:w-1/2 text-center md:text-left">
+                <h3 className="text-green-600 text-[30px] font-semibold">
+                  FRESH GROCERY
+                </h3>
+                <h2 className="text-3xl md:text-4xl lg:text-6xl font-extrabold my-2 leading-[1.5] -tracking-wide">
+                  There's you can Buy your all of Grocery Products.
+                </h2>
+                <PrimaryButton
+                  text="Shop Now"
+                  colour="[#FFBB38]"
+                  hoverColour="black"
                 />
               </div>
+              <div className="md:w-1/2 flex justify-center">
+                <div className="h-[450px] w-[450px]">
+                  <img src={heroImgOne} alt="Grocery" className="" />
+                </div>
+              </div>
             </div>
-          </div>
-        </Slider>
-      </div>
+
+            {/* Slide 2 */}
+            <div className="!flex flex-col-reverse md:flex-row place-items-center w-full p-6">
+              <div className="md:w-1/2 text-center md:text-left">
+                <h3 className="text-green-600 text-[30px] font-semibold">
+                  HEALTHY FOODS
+                </h3>
+                <h2 className="text-3xl md:text-4xl lg:text-6xl font-extrabold my-2 leading-[1.5] -tracking-wide">
+                  We Provide Fresh And Organic Food to Your Door.
+                </h2>
+                <PrimaryButton
+                  text="Shop Now"
+                  colour="[#FFBB38]"
+                  hoverColour="black"
+                />
+              </div>
+              <div className="md:w-1/2 flex justify-center">
+                <div className="max-h-[450px] mx-auto max-w-[450px]">
+                  <img
+                    src={heroImgTwo}
+                    alt="Grocery"
+                    className="w-full h-auto"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Slide 3 */}
+            <div className="!flex flex-col-reverse md:flex-row w-full place-items-center p-6">
+              <div className="md:w-1/2 text-center md:text-left">
+                <h3 className="text-green-600 text-[30px] font-semibold">
+                  HEALTHY FOODS
+                </h3>
+                <h2 className="text-3xl md:text-4xl lg:text-6xl font-extrabold my-2 leading-[1.5] -tracking-wide">
+                  You Can Buy All The Grocery Items Hassle-Free.
+                </h2>
+                <PrimaryButton
+                  text="Shop Now"
+                  colour="[#FFBB38]"
+                  hoverColour="black"
+                />
+              </div>
+              <div className="md:w-1/2 flex justify-center">
+                <div className="max-h-[450px] mx-auto max-w-[450px]">
+                  <img
+                    src={heroImgTheree}
+                    alt="Grocery"
+                    className="w-full h-auto"
+                  />
+                </div>
+              </div>
+            </div>
+          </Slider>
+        </div>
+      </SectionContainer>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import CarouselSlider from "../components/Slider/CarouselSlider";
 import SectionContainer from "../components/SectionContainer/SectionContainer";
 import FeaturesSection from "../components/FetureSections/FetureSection";
+import bgImage from "../../public/assets/new-arrival-bg.webp";
 
 const Home = () => {
   return (
     <div>
-      <SectionContainer>
-        <CarouselSlider />
-      </SectionContainer>
+      <div
+        className="h-full opacity-100 w-full bg-cover bg-center"
+        style={{ backgroundImage: `url('/assets/new-arrival-bg.webp')` }}
+      >
+        <div className="z-10 relative">
+          <CarouselSlider />
+        </div>
+      </div>
+
       <FeaturesSection />
     </div>
   );
