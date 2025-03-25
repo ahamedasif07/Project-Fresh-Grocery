0f766dcfff371422d9cee59a769d1c048dc368ba
rxasif07 <rxasif31@gmail.com>
2025-03-25 10:19:27 +0600

    container done

--- src/components/MarketCategory/MarketCategory.jsx
+++ src/components/MarketCategory/MarketCategory.jsx
@@ -56,7 +56,9 @@ const MarketCategory = () => {
                 <div className="bg-[#F3FAF5] border-transparent hover:border-[1px] transition-all duration-300 ease-in-out  hover:border-green-600 h-[200px] w-[200px] flex justify-center items-center rounded-xl">
                   <img src={category.image}></img>
                 </div>
-                <p>{category.name}</p>
+                <p className="text-[20px] text-gray-600 font-semibold text-center mt-[15px]">
+                  {category.name}
+                </p>
               </div>
             </div>
           ))}
