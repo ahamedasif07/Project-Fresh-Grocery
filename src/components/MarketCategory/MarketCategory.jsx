65a10ba69e6a03778a7fd48737f2b453389a2219
rxasif07 <rxasif31@gmail.com>
2025-03-25 10:17:19 +0600

    image fixed

--- src/components/MarketCategory/MarketCategory.jsx
+++ src/components/MarketCategory/MarketCategory.jsx
@@ -6,38 +6,39 @@ const MarketCategory = () => {
     {
       id: 1,
       name: "Fruits",
-      image: "/path-to-images/fruits.png",
+      image: "https://i.ibb.co.com/VYmbyDp3/c-img-1.webp",
     },
     {
       id: 2,
       name: "Vegetable",
-      image: "/path-to-images/vegetable.png",
+      image: "https://i.ibb.co.com/tM516Bt1/c-img-2.webp",
     },
     {
       id: 3,
       name: "Juice",
-      image: "/path-to-images/juice.png",
+      image: "https://i.ibb.co.com/ZR5M1Gbr/c-img-3.webp",
     },
     {
       id: 4,
       name: "Meat",
-      image: "/path-to-images/meat.png",
+      image: "https://i.ibb.co.com/27KzjwBj/c-img-4.webp",
     },
     {
       id: 5,
       name: "Cold Drinks",
-      image: "/path-to-images/cold-drinks.png",
+      image: "https://i.ibb.co.com/yBSXhHbk/c-img-5.webp",
     },
     {
       id: 6,
       name: "Breads",
-      image: "/path-to-images/breads.png",
+      image: "https://i.ibb.co.com/MvKHvNt/c-img-6.webp",
     },
   ];
 
   return (
     <div>
       <SectionContainer>
+        {/* titel */}
         <div className="flex place-items-center justify-between">
           <h2 className="text-[28px] text-black font-semibold">
             Market Categorys
@@ -46,6 +47,19 @@ const MarketCategory = () => {
             <h2 className="text-[16px] text-black font-semibold">View All</h2>
             <p className="h-[2px] w-0 group-hover:w-[65px] transition-all duration-300 ease-in-out bg-black"></p>
           </div>
+        </div>
+        {/* catagory images */}
+        <div className="flex flex-wrap items-center justify-around mt-[40px]">
+          {categories.map((category) => (
+            <div key={category.id}>
+              <div>
+                <div className="bg-[#F3FAF5] border-transparent hover:border-[1px] transition-all duration-300 ease-in-out  hover:border-green-600 h-[200px] w-[200px] flex justify-center items-center rounded-xl">
+                  <img src={category.image}></img>
+                </div>
+                <p>{category.name}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </SectionContainer>
     </div>
