1ac2eb112cbe7d2dcbe698b05b1176d4b67857fc
rxasif07 <rxasif31@gmail.com>
2025-04-08 08:32:28 +0600

    frist category doen

--- src/components/MainCategory/MainCategory.jsx
+++ src/components/MainCategory/MainCategory.jsx
@@ -6,20 +6,30 @@ import SectionContainer from "../SectionContainer/SectionContainer";
 import mainCategoryItemOne from "../../../public/assets/c-shop-img-1.webp";
 import mainCategoryItemTwo from "../../../public/assets/c-img-2.webp";
 import mainCategoryItemTheree from "../../../public/assets/c-img-3.webp";
+import PrimaryButton from "../PrymaryButton/PrimaryButton";
 
 const MainCategory = () => {
   return (
     <div>
       <SectionContainer>
         <div>
-          <div className="relative border-2 border-yellow-300">
+          <div className="relative border-2 overflow-hidden border-yellow-300">
             <img src={mainCategoryOne}></img>
 
-            <div className="absolute bottom-1">
+            <div className="absolute bottom-1  ">
               <div className="">
-                <div>
-                  <h4>FRUITS</h4>
-                  <h3>Healthy & Goods Fruits</h3>
+                <div className=" p-4">
+                  <h4 className="text-[15px] font-semibold">FRUITS</h4>
+                  <h3 className="text-3xl font-bold">
+                    Healthy & Goods <br /> Fruits
+                  </h3>
+                  <div className="mt-2">
+                    <PrimaryButton
+                      text="Shop Now"
+                      colour="green-600"
+                      hoverColour="black"
+                    ></PrimaryButton>
+                  </div>
                 </div>
                 <img src={mainCategoryItemOne}></img>
               </div>
