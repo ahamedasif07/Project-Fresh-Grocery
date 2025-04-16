c0073a5afbc495ecaea5e29c30203f8667d3c0d8
rxasif07 <rxasif31@gmail.com>
2025-04-16 08:09:16 +0600

    onae category card doen

--- src/components/MainCategory/MainCategory.jsx
+++ src/components/MainCategory/MainCategory.jsx
@@ -5,38 +5,62 @@ import mainCategoryThree from "../../../public/assets/c-bg-img-3.webp";
 import SectionContainer from "../SectionContainer/SectionContainer";
 import mainCategoryItemOne from "../../../public/assets/c-shop-img-1.webp";
 import mainCategoryItemTwo from "../../../public/assets/c-img-2.webp";
-import mainCategoryItemTheree from "../../../public/assets/c-img-3.webp";
+import mainCategoryItemTheree from "../../../public/assets/c-shop-img-3.webp";
 import PrimaryButton from "../PrymaryButton/PrimaryButton";
 
 const MainCategory = () => {
   return (
     <div>
       <SectionContainer>
-        <div>
-          <div className="relative border-2 overflow-hidden border-yellow-300">
-            <img src={mainCategoryOne}></img>
+        <div className="grid grid-cols-3 mx-auto mt-[30px] gap-3 rounded-md">
+          {/* image one */}
+          <div className="relative  overflow-hidden max-w-[400px] ">
+            <img className="" src={mainCategoryOne}></img>
 
-            <div className="absolute bottom-1  ">
-              <div className="">
-                <div className=" p-4">
-                  <h4 className="text-[15px] font-semibold">FRUITS</h4>
+            <div className="absolute bottom-1 overflow-hidden">
+              <div className=" space-y-[-35px]">
+                <div className="  p-4 space-y-[14px]  ">
+                  <h4 className="text-[15px]  mt-[30px] font-semibold">
+                    FRUITS
+                  </h4>
                   <h3 className="text-3xl font-bold">
                     Healthy & Goods <br /> Fruits
                   </h3>
-                  <div className="mt-2">
-                    <PrimaryButton
-                      text="Shop Now"
-                      colour="green-600"
-                      hoverColour="black"
-                    ></PrimaryButton>
-                  </div>
+                  <PrimaryButton
+                    text="Shop Now"
+                    colour="green-600"
+                    hoverColour="black"
+                  ></PrimaryButton>
                 </div>
                 <img src={mainCategoryItemOne}></img>
               </div>
             </div>
           </div>
+          {/* image tow */}
           <img src={mainCategoryTow}></img>
-          <img src={mainCategoryThree}></img>
+          {/* image 3 */}
+          <div className="relative  overflow-hidden max-w-[400px] ">
+            <img className="" src={mainCategoryThree}></img>
+
+            <div className="absolute bottom-1 overflow-hidden">
+              <div className=" space-y-[-35px]">
+                <div className="  p-4 space-y-[14px]  ">
+                  <h4 className="text-[15px]  mt-[30px] font-semibold">
+                    FRUITS
+                  </h4>
+                  <h3 className="text-3xl font-bold">
+                    Healthy & Goods <br /> Fruits
+                  </h3>
+                  <PrimaryButton
+                    text="Shop Now"
+                    colour="#921DFF"
+                    hoverColour="black"
+                  ></PrimaryButton>
+                </div>
+                <img src={mainCategoryItemTheree}></img>
+              </div>
+            </div>
+          </div>
         </div>
       </SectionContainer>
     </div>
