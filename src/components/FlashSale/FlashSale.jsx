5a575937fbc06a49775f106f4bebea7d8f536548
rxasif07 <rxasif31@gmail.com>
2025-04-23 08:32:26 +0600

    seles section  seding

--- src/components/FlashSale/FlashSale.jsx
+++ src/components/FlashSale/FlashSale.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import flashSale from "../../../public/assets/flash_sale.webp";
 import SectionContainer from "../SectionContainer/SectionContainer";
+import PrimaryButton from "../PrymaryButton/PrimaryButton";
 
 const FlashSale = () => {
   return (
@@ -11,7 +12,47 @@ const FlashSale = () => {
           style={{
             backgroundImage: `url(${flashSale})`,
           }}
-        ></div>
+        >
+          <div className="  py-10 px-4 md:px-[60px] ">
+            <div className="flex  gap-6 mb-8 flex-wrap">
+              {["Days", "Hours", "Minutes", "Seconds"].map((label, index) => {
+                const colors = [
+                  "text-red-500",
+                  "text-sky-500",
+                  "text-green-500",
+                  "text-red-500",
+                ];
+                return (
+                  <div
+                    key={label}
+                    className="bg-white w-24 h-24 rounded-full flex flex-col justify-center items-center shadow"
+                  >
+                    <span className={`text-3xl font-bold ${colors[index]}`}>
+                      0
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="py-[20px]">
+              <h2 className="text-5xl font-extrabold text-gray-900 my-4">
+                WOO! Flash Sale
+              </h2>
+              <p className="text-gray-900 max-w-xl text-[18px] my-[20px]">
+                You get into the 2k+ best Products in <br />
+                Flash offer with as in shaped sofa for sale.
+              </p>
+              <div>
+                <PrimaryButton
+                  text="Shop Now"
+                  colour="#34A853"
+                  hoverColour="#1D1D1D"
+                ></PrimaryButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </SectionContainer>
     </div>
   );
