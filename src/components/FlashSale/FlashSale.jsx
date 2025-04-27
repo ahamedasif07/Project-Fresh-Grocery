0d135046edec7f36b5122a75ef7448ff2b25bfba
rxasif07 <rxasif31@gmail.com>
2025-04-27 07:47:46 +0600

    discount baner set

--- src/components/FlashSale/FlashSale.jsx
+++ src/components/FlashSale/FlashSale.jsx
@@ -45,51 +45,58 @@ const FlashSale = () => {
             backgroundImage: `url(${flashSale})`,
           }}
         >
-          <div className="  py-10 px-4 md:px-[60px] ">
-            <div className="flex  gap-6 mb-8 flex-wrap">
-              {["Days", "Hours", "Minutes", "Seconds"].map((label, index) => {
-                const colors = [
-                  "text-red-500",
-                  "text-sky-500",
-                  "text-green-500",
-                  "text-red-500",
-                ];
-                const values = [
-                  timeLeft.days,
-                  timeLeft.hours,
-                  timeLeft.minutes,
-                  timeLeft.seconds,
-                ];
-                return (
-                  <div key={label}>
-                    <div className="bg-white w-24 h-24 rounded-full flex flex-col justify-center items-center shadow">
-                      <span className={`text-3xl font-bold ${colors[index]}`}>
-                        {values[index]}
-                      </span>
+          <div className="flex justify-between items-end  py-10 px-4 md:px-[60px]">
+            <div className="  ">
+              <div className="flex  gap-6 mb-8 flex-wrap">
+                {["Days", "Hours", "Minutes", "Seconds"].map((label, index) => {
+                  const colors = [
+                    "text-red-500",
+                    "text-sky-500",
+                    "text-green-500",
+                    "text-red-500",
+                  ];
+                  const values = [
+                    timeLeft.days,
+                    timeLeft.hours,
+                    timeLeft.minutes,
+                    timeLeft.seconds,
+                  ];
+                  return (
+                    <div key={label}>
+                      <div className="bg-white w-24 h-24 rounded-full flex flex-col justify-center items-center shadow">
+                        <span className={`text-3xl font-bold ${colors[index]}`}>
+                          {values[index]}
+                        </span>
+                      </div>
+                      <h3 className="text-[18px] font-semibold ml-[18px] mt-1">
+                        {label}
+                      </h3>
                     </div>
-                    <h3 className="text-[18px] font-semibold ml-[18px] mt-1">
-                      {label}
-                    </h3>
-                  </div>
-                );
-              })}
-            </div>
-
-            <div className="py-[8px] md:py-[20px]">
-              <h2 className="text-5xl font-extrabold text-gray-900 my-4">
-                WOO! Flash Sale
-              </h2>
-              <p className="text-gray-900 max-w-xl text-[18px] my-[20px]">
-                You get into the 2k+ best Products in <br />
-                Flash offer with as in shaped sofa for sale.
-              </p>
-              <div>
-                <PrimaryButton
-                  text="Shop Now"
-                  colour="#34A853"
-                  hoverColour="#1D1D1D"
-                ></PrimaryButton>
+                  );
+                })}
               </div>
+
+              <div className="py-[8px] md:py-[20px]">
+                <h2 className="text-5xl font-extrabold text-gray-900 my-4">
+                  WOO! Flash Sale
+                </h2>
+                <p className="text-gray-900 max-w-xl text-[18px] my-[20px]">
+                  You get into the 2k+ best Products in <br />
+                  Flash offer with as in shaped sofa for sale.
+                </p>
+                <div>
+                  <PrimaryButton
+                    text="Shop Now"
+                    colour="#34A853"
+                    hoverColour="#1D1D1D"
+                  ></PrimaryButton>
+                </div>
+              </div>
+            </div>
+            <div className="bg-yellow-500 flex justify-center items-center h-[200px] w-[200px] rounded-full text-5xl font-semibold">
+              26%
+              <br />
+              off
             </div>
           </div>
         </div>
