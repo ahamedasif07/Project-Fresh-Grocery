bd9ee5bc967bf7dda829afdfba99cdf6257f9f09
rxasif07 <rxasif31@gmail.com>
2025-04-30 09:32:18 +0600

    baner set with anamtion

--- src/components/TopSellProducts/TopSellProducts.jsx
+++ src/components/TopSellProducts/TopSellProducts.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import SectionContainer from "../SectionContainer/SectionContainer";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import TopSellingPRoductCard from "../TopSellingProductCard/TopSellingPRoductCard";
+import BanerOne from "../../../public/assets/grocery-bg.webp";
+import BanerTow from "../../../public/assets/grocery-bg-1.webp";
+import PrimaryButton from "../PrymaryButton/PrimaryButton";
 
 const TopSellProducts = () => {
   const products = [
@@ -62,6 +65,49 @@ const TopSellProducts = () => {
               ></TopSellingPRoductCard>
             ))}
           </div>
+          {/* top selling product baners  start*/}
+          <div className="flex justify-between items-center gap-10 py-[60px] ">
+            <div
+              data-aos="fade-right"
+              className="rounded-md bg-center bg-cover h-[300px] w-full"
+              style={{ backgroundImage: `url(${BanerOne})` }}
+            >
+              <div className="p-5">
+                <p className="text-[14px]  uppercase ">Fresh Vegetables</p>
+                <h3 className="text-4xl font-bold text-gray-900 py-5">
+                  The 14 Most Nutrient- Dense Vegetables
+                </h3>
+                <div className="mt-[30px]">
+                  <PrimaryButton
+                    text="Shop Now"
+                    colour="#FFBB38"
+                    hoverColour="#0A0A0A"
+                  ></PrimaryButton>
+                </div>
+              </div>
+            </div>
+
+            <div
+              data-aos="fade-left"
+              className="rounded-md bg-center bg-cover h-[300px] w-full "
+              style={{ backgroundImage: `url(${BanerTow})` }}
+            >
+              <div className="p-5">
+                <p className="text-[14px]  uppercase ">Fresh Fruits</p>
+                <h3 className="text-4xl font-bold text-gray-900 py-5">
+                  Healthy & Goods <br /> Fruits
+                </h3>
+                <div className="mt-[30px]">
+                  <PrimaryButton
+                    text="Shop Now"
+                    colour="#34A853"
+                    hoverColour="#0A0A0A"
+                  ></PrimaryButton>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* top selling product baners  end*/}
         </div>
       </SectionContainer>
     </div>
