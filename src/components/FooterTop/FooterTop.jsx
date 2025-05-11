4bee1583120f7f546ba2cf3642e766b3008d18d3
rxasif07 <rxasif31@gmail.com>
2025-05-11 08:16:06 +0600

    discount copun section design wit full responcive

--- src/components/FooterTop/FooterTop.jsx
+++ src/components/FooterTop/FooterTop.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import footerImage from "../../../public/assets/footer-top-bg.webp";
+import { MdOutlineMail } from "react-icons/md";
 
 const FooterTop = () => {
   return (
@@ -18,6 +19,23 @@ const FooterTop = () => {
               <p className="text-white text-[20px]">
                 by Subscribe our Newsletter
               </p>
+
+              {/* input filde */}
+              <div className="flex items-center ml-[50px] mt-[10px]">
+                <div className="flex items-center gap-3 w-[250px] md:w-[300px] px-6 py-[14px]  bg-white ">
+                  <span>
+                    <MdOutlineMail />
+                  </span>
+                  <input
+                    type="email"
+                    placeholder="EMAIL ADDRESS"
+                    className="outline-none w-full text-sm placeholder-black "
+                  />
+                </div>
+                <button className="bg-yellow-400 text-black px-3 md:px-6 h-[48px] font-semibold">
+                  Get the Coupon
+                </button>
+              </div>
             </div>
           </div>
         </div>
