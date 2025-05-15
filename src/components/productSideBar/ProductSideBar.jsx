16d3eb6c1fcbe9431ec8f557ffd64243fb03dcbb
rxasif07 <rxasif31@gmail.com>
2025-05-15 09:19:58 +0600

    sidebar done

--- src/components/productSideBar/ProductSideBar.jsx
+++ src/components/productSideBar/ProductSideBar.jsx
@@ -18,7 +18,21 @@ const ProductSideBar = () => {
     "Protein",
   ];
 
-  const brands = ["Refined Threads", "Ethereal Chic", "Yellow"];
+  const brands = [
+    "Refined Threads",
+    "Ethereal Chic",
+    "Yellow",
+    "Ecstasy",
+    "Urban Hive",
+    "Velvet Vista",
+    "Boldly Blue",
+    "Minted Mode",
+    "Eclectic Ensemble",
+    "BraveAlchemy Attire",
+    "Cascade Couture",
+  ];
+
+  const weights = ["500gm", "1kg", "2kg", "5kg", "10kg"];
 
   return (
     <div className="w-65 p-4 bg-white shadow rounded space-y-6 text-sm">
@@ -39,13 +53,25 @@ const ProductSideBar = () => {
       <DualRangeSlider />
 
       {/* Brands */}
-      <div>
+      <div className="pt-[20px]">
         <h2 className="font-semibold text-[18px] mb-2">Brands</h2>
         <div className="space-y-2">
           {brands.map((brand, idx) => (
-            <label key={idx} className="flex items-center space-x-2">
-              <input type="checkbox" className="accent-green-500" />
+            <label key={idx} className="flex items-center  space-x-2">
+              <input type="checkbox" className="accent-green-500  my-[23px]" />
               <span>{brand}</span>
+            </label>
+          ))}
+        </div>
+      </div>
+      {/* weights */}
+      <div className="pt-[20px]">
+        <h2 className="font-semibold text-[18px] mb-2">Weights</h2>
+        <div className="space-y-2">
+          {weights.map((weight, idx) => (
+            <label key={idx} className="flex items-center  space-x-2">
+              <input type="checkbox" className="accent-green-500  my-[23px]" />
+              <span>{weight}</span>
             </label>
           ))}
         </div>
